810ade78ee5c406dd05950bbf1fe7668384921ee
John <patrick.meyerin@gmail.com>
2025-03-17 15:03:03 -0500

    small cleanups

--- keycloak-api/src/main/java/coop/stlma/tech/protocolsn/keycloak/client/KeycloakAdminClient.java
+++ keycloak-api/src/main/java/coop/stlma/tech/protocolsn/keycloak/client/KeycloakAdminClient.java
@@ -7,13 +7,11 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Put;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.http.client.annotation.Client;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
--- keycloak/schema.sql
+++ keycloak/schema.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE keycloak;
 
 CREATE DATABASE nodemanager;
+
+CREATE DATABASE blogplugin;
